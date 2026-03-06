7898f35eaec453266595656c69ceaa93b6748299
Nyan Min Htet <nyanminhtet2304@gmail.com>
2026-03-06 15:47:27 +0630

    Delete UserMapper

--- src/main/java/dev/jade/expensetracker/domain/user/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package dev.jade.expensetracker.domain.user;
-
-import dev.jade.expensetracker.config.MapStructConfig;
-import org.mapstruct.Mapper;
-
-@Mapper(config = MapStructConfig.class)
-public interface UserMapper {
-
-}
